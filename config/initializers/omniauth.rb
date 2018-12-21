4e4e35278c627aa502ee6c9e8077fd37afdcc10f
Abhishek Kanojia <abhishek.kanojia@vinsol.com>
2018-12-21 18:37:41 +0530

    Add environment variable.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, Rails.application.credentials.oauth[:linked_in_client_id], Rails.application.credentials.oauth[:linked_in_client_secret], secure_image_url: true, provider_ignores_state: true
+  provider :linkedin, ENV['linked_in_client_id'], ENV['linked_in_client_secret'], secure_image_url: true, provider_ignores_state: true
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
