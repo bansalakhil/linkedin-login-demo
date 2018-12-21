e0a22143b155c45a4630eb1f1a78b263c2850ec4
Abhishek Kanojia <abhishek.kanojia@vinsol.com>
2018-12-21 19:07:42 +0530

    Update application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  http_basic_authenticate_with name: Rails.application.credentials.user_name, password: Rails.application.credentials.app_password
+  http_basic_authenticate_with name: ENV['user_name'], password: ENV['app_password']
 
 end
